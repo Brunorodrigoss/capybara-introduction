f1bf4154161836ed07679bddd9adc4f60a93043a
Bruno <bruno.soares@dextra-sw.com>
2019-05-26 07:27:51 -0300

    Class 1

---
+++ spec/hello_spec.rb
@@ -0,0 +1,6 @@
+describe 'Meu primeiro script' do
+    it 'visitar a pagina' do
+        visit 'https://training-wheels-protocol.herokuapp.com/'
+        expect(page.title).to eql 'Training Wheels Protocols'
+    end
+end
