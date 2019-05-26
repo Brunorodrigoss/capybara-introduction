ea04047150fe5cde7c5657bee0beaf1449898bb8
Bruno <bruno.soares@dextra-sw.com>
2019-05-26 13:14:55 -0300

    Capybara setup

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,6 +1,8 @@
+
+
 describe 'Meu primeiro script' do
     it 'visitar a pagina' do
         visit 'https://training-wheels-protocol.herokuapp.com/'
-        expect(page.title).to eql 'Training Wheels Protocols'
+        expect(page.title).to eql 'Training Wheels Protocol'
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,5 +16,6 @@ RSpec.configure do |config|
 end
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  # config.default_driver = :selenium_chrome
+  config.default_driver = :selenium
 end
